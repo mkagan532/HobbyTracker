d272cd928d84581c2d9ffe488d01d570bd11bcef
Michael Kagan <mkagan532@gmail.com>
2020-01-30 21:40:50 -0500

    Initial Commit

---
+++ HobbyTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HobbyTracker
+//
+//  Created by Lambda_School_Loaner_232 on 1/30/20.
+//  Copyright © 2020 Michael Kagan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
